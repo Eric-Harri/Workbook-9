7e28063f340ed8153e6cebcda44ce7a6b502026b
Eric Harris <ericoldie@gmail.com>
2022-11-21 16:14:17 -0500

    code exercises completed

--- exercises/extend_class_exercise.js
+++ exercises/extend_class_exercise.js
@@ -13,13 +13,21 @@ let person = new Person("Eric", "Harris");
 console.log(person.getFullName());
 
 class Employee extends Person {
-    constructor(id, firstName, lastName, jobTitle, payRate) {
-        super(firstName, lastName)
-        this.employeeId = id;
-        this.jobTitle = jobTitle;
-        this.payRate = payRate;
-    };
+  constructor(id, firstName, lastName, jobTitle, payRate) {
+    super(firstName, lastName);
+    this.employeeId = id;
+    this.jobTitle = jobTitle;
+    this.payRate = payRate;
+  }
+
+  getPayStub() {
+    return `Employee Name: ${this.getFullName()}
+            Job Title: ${this.jobTitle}
+            Bi-Weekly: ${(this.payRate * 40) * 2}
+            Salary: ${(this.payRate * 40) * 52}`
+  }
 }
-let employee = new Employee(1, "Eric", "Harris", "Software Developer", 30.5)
+let employee = new Employee(1, "Eric", "Harris", "Software Developer", 31.5);
 
 console.log(employee);
+console.log(employee.getPayStub());
