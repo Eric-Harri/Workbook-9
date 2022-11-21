cc00c161cd7240deacd392c3ec8b95e09bc7e311
Eric Harris <ericoldie@gmail.com>
2022-11-21 11:35:59 -0500

    First exercises concerning the keyword this, class

---
+++ demos/class1.js
@@ -0,0 +1,58 @@
+let person = {
+    first: "Jarel",
+    last: "Wright",
+    email: "nottoday012@email.com"
+};
+
+console.log(person)
+
+let person2 = {
+    first: "Ari",
+    last: "Black",
+    email: "blackisback@email.com"
+};
+
+console.log(person2)
+
+class Person {
+    first;
+    last;
+    email; 
+}
+
+let person3 = new Person()
+person3.first = "Rayna";
+person3.last = "Bell";
+person3.email = "DoIringaBell@email.com"
+
+console.log(person3)
+
+let person4 = new Person()
+person4.first = "Craig"
+person4.last = "Mceachie"
+person4.email = "MrMan@email.com"
+
+console.log(person4)
+
+let person5 = new Person()
+person5.first = "Samuel"
+person5.last = "Kahura"
+person5.email = "SKahura@email.com"
+
+console.log(person5)
+
+class Person1 {
+//   first;
+//   last;
+//   email;
+    constructor(first, last, email) {
+        this.first = first;
+        this.last = last;
+        this.email = email; 
+  }
+    
+    
+}
+
+let person7 = new Person1("Rayna", "Star", "RainStar@email.com")
+console.log(person7)
